5888b20c0a646fc0e81cb280a3893b6074ed203b
w0nyv <nagano.tamawo77@gmail.com>
2023-02-16 00:47:03 +0900

    first commmit

---
+++ web-socket-tutorial.js/app.js
@@ -0,0 +1,27 @@
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+
+//この文法脳がバグる
+const io = require('socket.io')(http);
+
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+http.listen(PORT, () => console.log('server listening. Port' + PORT));
+
+app.get('/', (req, res, next) => res.render("index", {}));
+
+io.on('connection', (socket) => {
+
+    //イベントの検知
+    socket.on('sendMessage', (msg) => {
+
+        console.log('message :' + msg);
+
+        //イベントの発火
+        io.emit('receiveMessage', msg);
+
+    });
+});
